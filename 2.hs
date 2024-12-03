e1af428b186c811a387a5047e945c8e85e707e15
nathan patel <nathan.patel@benevolent.ai>
2024-12-02 21:18:52 -0800

    improve day 2

--- 2.hs
+++ 2.hs
@@ -1,31 +1,20 @@
 import Parser ( Parser, integer, newline, runParser' )
-import Control.Applicative (Alternative (..))
-import Data.Either (fromRight)
-import Data.List ( group, sort )
-import qualified Data.IntMap as Map
+import Control.Applicative (Alternative (..), liftA2)
 
 parse :: Parser [[Int]]
 parse = some (some integer <* newline)
 
 mainA :: IO ()
-mainA = do
-    print . length . filter isSafe . map pairDiff . runParser' parse =<< getContents
+mainA = print . length . filter isSafe . runParser' parse =<< getContents
 
 mainB :: IO ()
-mainB = do
-    options <- map options . runParser' parse <$> getContents
-    print . length . filter (not . null) $ filter isSafe . map pairDiff <$> options
+mainB = print . length . filter (any isSafe) . map options . runParser' parse =<< getContents
+  where options l = l : map (`removeAt` l) [0..(length l-1)]
+        removeAt n = take n <> drop (n+1)
 
 main = mainB
 
-pairDiff :: [Int] -> [Int]
-pairDiff l = zipWith (flip (-)) l (tail l)
-
 isSafe :: [Int] -> Bool
-isSafe l = all (`elem` [-1,-2,-3]) l || all (`elem` [1,2,3]) l
-
-removeAt :: Int -> [a] -> [a]
-removeAt n l = take n l ++ drop (n+1) l
-
-options :: [a] -> [[a]]
-options l = l : map (`removeAt` l) [0..(length l-1)]
+isSafe = gradual . deltas
+  where deltas = zipWith (-) <*> tail
+        gradual  = (||) <$> all (`elem` [-1,-2,-3]) <*> all (`elem` [1,2,3])
