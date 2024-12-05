39fc60e859fc5313af2947f08607b9141b72c215
nathan patel <nathan.patel@benevolent.ai>
2024-12-04 17:40:07 -0800

    small fixes

--- 4.hs
+++ 4.hs
@@ -6,29 +6,29 @@ import Data.List (isPrefixOf)
 
 type Input = Map (Int, Int) Char
 
+instance (Num a, Num b) => Num (a, b) where
+    (+) (x1, y1) (x2, y2) = (x1+x2, y1+y2)
+
 parser :: Parser Input
 parser = makeMap <$> some (some (oneOf "XMAS") <* newline)
-
-makeMap :: [[Char]] -> Map (Int, Int) Char
-makeMap = Map.fromList . concat . zipWith (\y -> map (\(x, c) -> ((x, y), c))) [0..] . map (zip [0..])
+    where makeMap = Map.fromList . concat . zipWith (\y -> map (\(x, c) -> ((x, y), c))) [0..] . map (zip [0..])
 
 part1 :: Input -> Int
 part1 = length . findOccurances "XMAS"
 
+findOccurances :: String -> Map (Int, Int) Char -> [(((Int, Int), (Int, Int)), String)]
 findOccurances word grid = filter (isPrefixOf word . snd) $ allPaths grid
-allPaths grid = (zip <*> map (uncurry (pathInDir grid))) ((,) <$> directions <*> Map.keys grid)
-directions  = [(x, y) | x<-[-1..1], y<-[-1..1], x/=0 || y/=0]
-pathInDir  grid d p | p `Map.member` grid = (grid ! p) : pathInDir grid d (p `add` d)
-                    | otherwise = []
-add (x1, y1) (x2, y2) = (x1+x2, y1+y2)
-
+    where allPaths grid = (zip <*> map (pathInDir grid)) ((,) <$> directions <*> Map.keys grid)
+          directions  = [(x, y) | x<-[-1..1], y<-[-1..1], x/=0 || y/=0]
+          pathInDir  grid (d, p) | p `Map.member` grid = (grid ! p) : pathInDir grid (d, p + d)
+                                 | otherwise = []
 
 part2 :: Input -> Int
 part2 = (`div` 2) . length . filter (uncurry isCrossing) . combos2 . findOccurances "MAS"
     where combos2 l = [(a, b) | a <- l, b <- l]
           isCrossing ((d1, p1), _) ((d2, p2), _) = c1 == c2 && areOrthogonal d1 d2 && isDiagonal d1
-                where c1 = d1 `add` p1
-                      c2 = d2 `add` p2
+                where c1 = d1 + p1
+                      c2 = d2 + p2
           isDiagonal (dx, dy) = dx /= 0 && dy /= 0
           areOrthogonal d1 d2 = d1 == rotate90 d2 || d1 == rotate270 d2
           rotate90 (dx, dy) = (-dy, dx)
